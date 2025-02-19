c01915ee6f77f21eae0e62631427f0824fd52ff5
mkocovic2 <michaelk2ny@gmail.com>
2025-02-18 20:02:00 -0500

    Added basic start, just some simple print statements

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main{
   public static void main(String[] args) {
-        
+    System.out.println("Welcome to the library!");
+    System.out.println("Choose on of the following options (number)");
+
   }
 }
