41cbabcd0fb48945784c5b2d4e5bbd8ac25a081c
Michael Kocovic <michaelk2ny@gmail.com>
2025-02-22 18:28:21 -0500

    Added Instructions print out to Main.java, added LibraryManager class, not populated yet.

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 class Main{
   public static void main(String[] args) {
-    System.out.println("Welcome to the library!");
-    System.out.println("Choose on of the following options (number)");
-    
+    Scanner userInput = new Scanner(System.in);
+    int answer = 0; 
+    System.out.println("Welcome to the library!\n");
+    while(answer != 7){
+      printInstructions();
+      answer = userInput.nextInt();
+    }    
+  }
+
+  static void printInstructions(){
+    System.out.println("Choose on of the following options (number)\n");
+    System.out.println("1. Display all books");
+    System.out.println("2. Display all users");
+    System.out.println("3. User information");
+    System.out.println("4. Book information");
+    System.out.println("5. Borrowing Books");
+    System.out.println("6. Returning Books (Advanced)");
+    System.out.println("7. Quit");
   }
 }
